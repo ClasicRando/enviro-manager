a108cbef397a6525a1ccd9b2bfb655002a639adf
Steven Thomson <steventhomson9@gmail.com>
2024-07-05 09:51:47 -0400

    chore: remove BasePage template

--- web/src/main/kotlin/com/github/clasicrando/web/Utils.kt
+++ web/src/main/kotlin/com/github/clasicrando/web/Utils.kt
@@ -2,48 +2,16 @@ package com.github.clasicrando.web
 
 import com.github.clasicrando.users.data.UsersDao
 import com.github.clasicrando.users.model.User
-import com.github.clasicrando.web.htmx.SwapType
-import com.github.clasicrando.web.htmx.hxGet
-import com.github.clasicrando.web.htmx.hxSwap
-import com.github.clasicrando.web.htmx.hxTrigger
 import com.github.clasicrando.web.htmx.respondHtmx
-import com.github.clasicrando.web.page.BasePage
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.html.respondHtmlTemplate
 import io.ktor.server.request.ApplicationRequest
 import io.ktor.server.response.respondRedirect
 import io.ktor.server.sessions.get
 import io.ktor.server.sessions.sessions
-import kotlinx.html.div
-import kotlinx.html.id
 
 const val MAIN_CONTENT_ID = "main"
 const val MAIN_CONTENT_TARGET = "#$MAIN_CONTENT_ID"
 const val NO_DISPLAY_ELEMENT_TARGET = "#noDisplay"
-
-suspend inline fun ApplicationCall.respondBasePage(
-    contentUrl: String,
-    user: User? = null,
-    stylesheetHref: String? = null,
-    pageTitle: String? = null,
-) {
-    val template =
-        BasePage(
-            user = user,
-            stylesheetHref = stylesheetHref,
-            pageTitle = pageTitle,
-        )
-    respondHtmlTemplate(template) {
-        innerContent {
-            div {
-                id = MAIN_CONTENT_ID
-                hxGet = contentUrl
-                hxTrigger = "load"
-                hxSwap(SwapType.InnerHtml)
-            }
-        }
-    }
-}
 
 suspend fun ApplicationCall.userSessionOrRedirect(): UserSession? {
     val userSession = sessions.get<UserSession>()
